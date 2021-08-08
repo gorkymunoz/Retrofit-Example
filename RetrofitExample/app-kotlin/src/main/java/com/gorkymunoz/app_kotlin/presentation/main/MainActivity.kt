48fc921416b1d98ec37e36781b5df78452263390
Gorky Muñoz <gorkymunoz@hotmail.com>
2021-08-08 16:05:01 -0500

    show list of characters

--- RetrofitExample/app-kotlin/src/main/java/com/gorkymunoz/app_kotlin/presentation/main/MainActivity.kt
+++ RetrofitExample/app-kotlin/src/main/java/com/gorkymunoz/app_kotlin/presentation/main/MainActivity.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gorkymunoz.app_kotlin.data.repositories.CharacterRepository
 import com.gorkymunoz.app_kotlin.data.usecase.GetAllCharactersUseCase
 import com.gorkymunoz.app_kotlin.databinding.ActivityMainBinding
 import com.gorkymunoz.app_kotlin.network.RetrofitClient
 import com.gorkymunoz.app_kotlin.network.model.character.CharacterList
-import com.gorkymunoz.app_kotlin.presentation.ResultUI
+import com.gorkymunoz.app_kotlin.presentation.common.ResultUI
 import com.gorkymunoz.app_kotlin.presentation.common.getViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
+    private val characterAdapter by lazy { CharacterAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +35,12 @@ class MainActivity : AppCompatActivity() {
             it.getCharacterList()
         }
 
+        binding.rvCharacter.apply {
+            layoutManager =
+                GridLayoutManager(this@MainActivity, 3, LinearLayoutManager.VERTICAL, false)
+            adapter = characterAdapter
+        }
+
         viewModel.characters.observe(this, Observer(::updateUI))
     }
 
@@ -40,8 +49,12 @@ class MainActivity : AppCompatActivity() {
         when (result) {
             is ResultUI.Loading -> print("LOADING")
             is ResultUI.Error -> Log.e("ERROR", result.exception.message, result.exception)
-            is ResultUI.Success -> Log.d("SUCCESS", result.data.toString())
+            is ResultUI.Success -> updateCharacters(result.data)
         }
+    }
+
+    private fun updateCharacters(data: CharacterList) {
+        characterAdapter.submitList(data.listOfCharacters)
     }
 
 }
